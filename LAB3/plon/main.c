851e2d819938cde6353d4a4867eda269500c062b
Rafal <rafal.sz@tutanota.com>
2020-11-15 12:26:44 +0100

    Deleted useless files, modified one program

--- LAB3/plon/main.c
+++ LAB3/plon/main.c
@@ -12,11 +12,11 @@
 #include <time.h>
 #include <ctype.h>
 
-void printCount(int wasArg, unsigned char startingChar, blksize_t count)
+void printCount(unsigned char startingChar, blksize_t count)
 {
-    if ((wasArg && !isprint(startingChar)) || (!wasArg && startingChar != 0 && !isprint(startingChar)))
+    if (!isprint(startingChar))
         printf("%d: %ld\n", startingChar, count);
-    else if (isprint(startingChar))
+    else
         printf("'%c': %ld\n", startingChar, count);
 }
 
@@ -57,24 +57,46 @@ int main(int argc, char* argv[])
     unsigned char* buffer = (unsigned char*)calloc(sizeof(unsigned char), blockSize);
 
     off_t prevOffset = lseek(fd, 0, SEEK_SET);
-    while ((prevOffset = lseek(fd, prevOffset, SEEK_DATA)) != -1)
+    if (!wasArg)
     {
-        read(fd, buffer, blockSize);
-        unsigned char startingChar = buffer[0];
-        blksize_t count = 1;
-        for (int i = 1; i < blockSize; i++)
+        while ((prevOffset = lseek(fd, prevOffset, SEEK_DATA)) != -1)
         {
-            if (buffer[i] == startingChar)
-                count++;
-            else
+            read(fd, buffer, blockSize);
+            unsigned char startingChar = buffer[0];
+            blksize_t count = 1;
+            for (int i = 1; i < blockSize; i++)
             {
-                printCount(wasArg, startingChar, count);
-                startingChar = buffer[i];
-                count = 1;
+                if (buffer[i] == startingChar)
+                    count++;
+                else
+                {
+                    printCount(startingChar, count);
+                    startingChar = buffer[i];
+                    count = 1;
+                }
+            }
+            printCount(startingChar, count);
+            prevOffset = lseek(fd, prevOffset, SEEK_HOLE);
+        }
+    }
+    else
+    {
+        // Associative array, initialized with 0's
+        int array[256] = {0};
+        while (read(fd, buffer, blockSize) > 0) 
+        {
+            for (int i = 0; i < blockSize; i++) 
+            {
+                array[buffer[i]]++;
             }
         }
-        printCount(wasArg, startingChar, count);
-        prevOffset = lseek(fd, prevOffset, SEEK_HOLE);
+        for (int i = 0; i < 256; i++)
+        {
+            if (array[i] != 0)
+            {
+                printCount(i, array[i]);
+            }
+        }
     }
 
     free(buffer);
