01c0fd4158f8f298a0779b5e03346ea11c1bd9f5
Rafal <rafal.sz@tutanota.com>
2020-11-21 18:09:02 +0100

    Added lab4 (descriptors)

--- LAB4/main.c
+++ LAB4/main.c
@@ -2,33 +2,52 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define MAX 1024
+
+char* GetReadWriteFlag(int flags)
+{
+    char* result = NULL;
+    switch (flags) {
+    case O_RDONLY:
+        result = "O_RDONLY";
+        break;
+    case O_WRONLY:
+        result = "O_WRONLY";
+        break;
+    case O_RDWR:
+        result = "O_RDWR";
+        break;
+    default:
+        result = "wtf";
+    }
+}
 
 int main()
 {
     for (int i = 0; i < MAX; i++)
     {
-        char mode[64];
         int flags = fcntl(i, F_GETFL);
         if (flags == -1)
             continue;
-        flags = flags & O_ACCMODE;
-        switch (flags) {
-        case O_RDONLY:
-            strcpy(mode, "O_RDONLY");
-            break;
-        case O_WRONLY:
-            strcpy(mode, "O_WRONLY");
-            break;
-        case O_RDWR:
-            strcpy(mode, "O_RDWR");
-            break;
-        default:
-            strcpy(mode, "wtf");
+        char* flag = GetReadWriteFlag(flags & O_ACCMODE);
+        printf("Deskryptor %d: %s", i, flag);
+        
+        if (flags & O_APPEND) {
+            printf(" | O_APPEND");
         }
-
-        printf("Deskryptor %d: %s\n", i, mode);
+        if (flags & O_ASYNC) {
+            printf(" | O_ASYNC");
+        }
+        if (flags & O_SYNC) {
+            printf(" | O_SYNC");
+        }
+        if (flags & O_NONBLOCK) {
+            printf(" | O_NONBLOCK");
+        }
+        printf("\n");
     }
     return 0;
 }
